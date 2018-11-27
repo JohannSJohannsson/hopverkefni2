7b9b48d088180ca650316ed00fa1d063edee460f
JohannSJohannsson <jsj13@hi.is>
2018-11-27 17:26:19 +0000

    header byrjun

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,21 @@
-export function empty(element){
+export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
-    }
   }
+}
+
+export function el(name, ...children) {
+  const element = document.createElement(name);
+
+  if (Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else if (child) {
+        element.appendChild(child);
+      }
+    });
+  }
+
+  return element;
+}
