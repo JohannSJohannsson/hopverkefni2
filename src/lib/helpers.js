baa935c6577506dc206f8f2921ff5471b9b7da6e
arg49 <arnthorgu@gmail.com>
2018-11-27 23:22:01 +0000

    Lagaði alskonar villur og breytti aðferð við að sækja data

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
-=======
-// eslint-disable-next-line import/prefer-default-export
->>>>>>> 60c63589d7aee4c3e89209a60c461e182b953cb6
 export function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
 }
-<<<<<<< HEAD
-
-export function el(name, ...children) {
-  const element = document.createElement(name);
-
-  if (Array.isArray(children)) {
-    children.forEach((child) => {
-      if (typeof child === 'string') {
-        element.appendChild(document.createTextNode(child));
-      } else if (child) {
-        element.appendChild(child);
-      }
-    });
-  }
-
-  return element;
-}
-=======
->>>>>>> 60c63589d7aee4c3e89209a60c461e182b953cb6
--- src/lib/list.js
+++ src/lib/list.js
@@ -8,54 +8,59 @@ export default class List {
   }
 
   loadLectures() {
-    fetch(this.url).then(res => res.json()).then((data) => {
-      // Work with JSON data here
-      console.log(data); /*eslint-disable-line*/ 
-      this.add(data);
-    }).catch((err) => {
-      console.log('error'); /*eslint-disable-line*/
-    });
-  }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var myArr = JSON.parse(this.responseText);
+            console.log(myArr['lectures']);
+            add(myArr);
+        }
+    };
+    xhttp.open("GET", this.url, true);
+    xhttp.send();
+    
+}
 
-  add(data) {
-    for (let i = 0; i < data.lectures.length; i += 1) {
-      const row = document.querySelector('.lectures__row');
-      const col = document.createElement('div');
-      col.setAttribute('class', 'lectures__col');
-      const link = document.createElement('a');
-      link.setAttribute('href', 'fyrirlestur.html');
-      const image = document.createElement('div');
-      image.setAttribute('class', 'lectures__image');
-      const img = document.createElement('img');
-      if (data.lectures[i].thumbnail) {
-        img.setAttribute('src', data.lectures[i].thumbnail);
-        img.setAttribute('alt', data.lectures[i].title);
-      }
-
-      const banner = document.createElement('div');
-      banner.setAttribute('class', 'lectures__banner');
-      const chapter = document.createElement('p');
-      chapter.setAttribute('class', 'lectures__chapter');
-      const cat = document.createTextNode(data.lectures[i].category);
-      chapter.appendChild(cat);
-      const title = document.createElement('h2');
-      title.setAttribute('class', 'lectures__title');
-      const tit = document.createTextNode(data.lectures[i].title);
-      title.appendChild(tit);
-
-
-      row.appendChild(col);
-      col.appendChild(link);
-      link.appendChild(image);
-      image.appendChild(img);
-      link.appendChild(banner);
-      banner.appendChild(chapter);
-      banner.appendChild(title);
-    }
-  }
 
   load() {
     empty(this.container);
     this.loadLectures();
+  }
+}
+
+function add(data) {
+  for (let i = 0; i < data.lectures.length; i += 1) {
+    const row = document.querySelector('.lectures__row');
+    const col = document.createElement('div');
+    col.setAttribute('class', 'lectures__col');
+    const link = document.createElement('a');
+    link.setAttribute('href', 'fyrirlestur.html');
+    const image = document.createElement('div');
+    image.setAttribute('class', 'lectures__image');
+    const img = document.createElement('img');
+    if (data.lectures[i].thumbnail) {
+      img.setAttribute('src', data.lectures[i].thumbnail);
+      img.setAttribute('alt', data.lectures[i].title);
+    }
+
+    const banner = document.createElement('div');
+    banner.setAttribute('class', 'lectures__banner');
+    const chapter = document.createElement('p');
+    chapter.setAttribute('class', 'lectures__chapter');
+    const cat = document.createTextNode(data.lectures[i].category);
+    chapter.appendChild(cat);
+    const title = document.createElement('h2');
+    title.setAttribute('class', 'lectures__title');
+    const tit = document.createTextNode(data.lectures[i].title);
+    title.appendChild(tit);
+
+
+    row.appendChild(col);
+    col.appendChild(link);
+    link.appendChild(image);
+    image.appendChild(img);
+    link.appendChild(banner);
+    banner.appendChild(chapter);
+    banner.appendChild(title);
   }
 }
