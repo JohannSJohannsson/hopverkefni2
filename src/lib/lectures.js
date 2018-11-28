de7b34c607bd1cc4022f089606e2a531b43d2afa
JohannSJohannsson <jsj13@hi.is>
2018-11-28 14:38:42 +0000

    Kominn með efnið að miklu leiti inn, búinn að laga mikið af eslint

--- src/lib/lectures.js
+++ src/lib/lectures.js
@@ -1,57 +1,121 @@
-/*eslint-disable*/
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable func-names */
 import { empty } from './helpers';
 
 export default class Lectures {
-    constructor() {
-      this.container = document.querySelector('.list');
-      this.url = '../lectures.json';
-      this.loadLectures();
-    }
-  
-    loadLectures() {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-              var myArr = JSON.parse(this.responseText);
-              console.log(myArr['lectures']);
-              createContent(myArr);
-          }
-      };
-      xhttp.open("GET", this.url, true);
-      xhttp.send();
-      
-  }
-  
-  
-    load() {
-      empty(this.container);
-    }
+  constructor() {
+    this.container = document.querySelector('.list');
+    this.url = '../lectures.json';
+    this.loadLectures();
   }
 
-function createContent(data) {
-    const params = new URLSearchParams(document.location.search);
-    const slug = params.get('slug');
+  loadLectures() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const myArr = JSON.parse(this.responseText);
+        console.log(myArr.lectures);
+        createHeader(myArr); /* eslint-disable-line */
+        createMain(myArr);  /* eslint-disable-line */
+      }
+    };
+    xhttp.open('GET', this.url, true);
+    xhttp.send();
+  }
 
-    for (let i in data.lectures) {
-      if (slug == data.lectures[i].slug) {
-        const header = document.querySelector('.header');
-        const content = document.querySelector('.header__content');
-        if (data.lectures[i].image) {
-            header.setAttribute('style', `background-image: url(../${  data['lectures'][i].image  })`);
+
+  load() {
+    empty(this.container);
+  }
+}
+
+function createHeader(data) {
+  const params = new URLSearchParams(document.location.search);
+  const slug = params.get('slug');
+
+  for (let i = 0; i < data.lectures.length; i += 1) {
+    if (slug === data.lectures[i].slug) {
+      const header = document.querySelector('.header');
+      const content = document.querySelector('.header__content');
+      if (data.lectures[i].image) {
+        header.setAttribute('style', `background-image: url(../${data.lectures[i].image})`);
+      } else {
+        header.setAttribute('style', 'background-color: #999');
+      }
+      const smalltext = document.createElement('p');
+      smalltext.setAttribute('class', 'header__smalltext');
+      const smltxt = document.createTextNode(data.lectures[i].category);
+      smalltext.appendChild(smltxt);
+      const title = document.createElement('h2');
+      const ttle = document.createTextNode(data.lectures[i].title);
+      title.setAttribute('class', 'header__title');
+      title.appendChild(ttle);
+      content.appendChild(smalltext);
+      content.appendChild(title);
+    }
+  }
+}
+
+function createMain(data) {
+  const params = new URLSearchParams(document.location.search);
+  const slug = params.get('slug');
+
+  for (let i = 0; i < data.lectures.length; i += 1) {
+    if (slug === data.lectures[i].slug) {
+      const efni = document.querySelector('.efni');
+      for (let x = 0; i < data.lectures.length; x += 1) {
+        const content = data.lectures[i].content; /* eslint-disable-line */
+        const divv = document.createElement('div'); // BREYTA
+        divv.setAttribute('class', 'lectures__main'); // redda þessu
+        let content1 = '';
+        let cdlist = '';
+        let cid = '';
+        switch (content[x].type) {
+          case 'youtube':
+            content1 = document.createElement('iframe');
+            content1.setAttribute('frameborder', 0);
+            content1.setAttribute('allowfullscreen', 0);
+            content1.setAttribute('src', content[x].data);
+            break;
+          case 'text':
+            content1 = document.createElement('p');
+            cid = document.createTextNode(content[x].data);
+            content1.appendChild(cid);
+            break;
+          case 'quote':
+            content1 = document.createElement('blockquote');
+            cid = document.createTextNode(content[x].data);
+            content1.appendChild(cid);
+            break;
+          case 'heading':
+            content1 = document.createElement('h2');
+            cid = document.createTextNode(content[x].data);
+            content1.appendChild(cid);
+            break;
+          case 'list':
+            content1 = document.createElement('ul');
+            // eslint-disable-next-line guard-for-in
+            for (const l in content[x].data) {
+              cdlist = document.createElement('li');
+              const cd = document.createTextNode(content[x].data[l]);
+              cdlist.appendChild(cd);
+              content1.appendChild(cdlist);
+            }
+            break;
+          case 'image':
+            content1 = document.createElement('img');
+            content1.setAttribute('src', content[x].data);
+
+            break;
+          case 'code':
+            content1 = document.createElement('code');
+            cid = document.createTextNode(content[x].data);
+            content1.appendChild(cid);
+            break;
+          default:
         }
-        else {
-            header.setAttribute('style', `background-color: #999`);
-        }
-        const smalltext = document.createElement('p');
-        smalltext.setAttribute('class', 'header__smalltext');
-        const smltxt = document.createTextNode(data.lectures[i].category);
-        smalltext.appendChild(smltxt);
-        const title = document.createElement('h2');
-        const ttle = document.createTextNode(data.lectures[i].title);
-        title.setAttribute('class', 'header__title');
-        title.appendChild(ttle);
-        content.appendChild(smalltext);
-        content.appendChild(title);
+        efni.appendChild(content1);
       }
     }
+  }
 }
